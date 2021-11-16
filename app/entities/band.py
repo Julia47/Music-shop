6bbff8fbe64d52f4440e04c6f8124c588982eb23
Julia Burmych <julia.burmich@gmail.com>
2021-11-16 19:33:08 +0200

    deleted old files

--- app/entities/band.py
+++
@@ -1,5 +0,0 @@
-class Band(object):
-
-    def __init__(self, id_band, name_band):
-        self.id_band = id_band
-        self.name_band = name_band
--- app/entities/permission.py
+++
@@ -1,7 +0,0 @@
-class Permission(object):
-
-    def __init__(self, permission, name, user):
-        self.id_permission = permission
-        self.name = name
-        self.user = user
-
